558c9cd83fe7ed48f6b6139c947e6f803d13b2f8
Cherie <terada@divertise.asia>
2024-01-28 18:34:21 +0700

    Add product manager models and migrations

--- cart_discount_module_app/product_manager/admin.py
+++ cart_discount_module_app/product_manager/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import ProductCategory, Product
 
-# Register your models here.
+
+class ProductCategoryAdminView(admin.ModelAdmin):
+    list_display = ("name",)
+
+
+class ProductAdminView(admin.ModelAdmin):
+    list_display = ("name", "description", "category", "price")
+    list_filter = ("category",)
+    search_fields = (
+        "name",
+        "description",
+    )
+
+
+admin.register(ProductCategory, ProductCategoryAdminView)
+admin.register(Product, ProductAdminView)
--- cart_discount_module_app/product_manager/models.py
+++ cart_discount_module_app/product_manager/models.py
@@ -1,3 +1,19 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100, blank=True, null=True)
+    category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+    price = models.FloatField(validators=[MinValueValidator(0)])
+
+    def __str__(self):
+        return self.name
